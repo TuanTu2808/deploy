e10a8a1c134d1c05e771405fa62327314ff65452
Le Thanh Tuan Tu <lethanhtuantu123@gmail.com>
2026-05-06 08:16:02 +0700

    thêm thời gian

--- app/components/booking_flow/TimeSlotPicker.tsx
+++ app/components/booking_flow/TimeSlotPicker.tsx
@@ -22,6 +22,7 @@ export default function TimeSlotPicker({
 >([]);
   const [loading, setLoading] = React.useState(false);
 const DEFAULT_TIMES = [
+  "08:00","08:30",
   "09:00","09:30","10:00","10:30",
   "11:00","11:30","12:00","12:30","13:00","13:30",
   "14:00","14:30","15:00","15:30","16:00","16:30",
